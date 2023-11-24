31d36941251ba5023de47bfdd9352c4b45fccf9f
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-24 03:33:35 +0300

    Update __init__.py

--- pyrogram/__init__.py
+++ pyrogram/__init__.py
@@ -16,9 +16,9 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "2.0.108"
+__version__ = "2.5.25"
 __license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
-__copyright__ = "Copyright (C) 2017-present Dan <https://github.com/delivrance>"
+__copyright__ = "Copyright (C) 2023-present Dan ASAAQ <https://github.com/Asaaq-ali>"
 
 from concurrent.futures.thread import ThreadPoolExecutor
 
